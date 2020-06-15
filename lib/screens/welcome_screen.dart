87d89c67af2c6c9bc45ca2c905cea2572c464e9b
nrrf <nrrojasfe@unal.edu.co>
2020-06-14 22:08:35 -0500

    New welcomeScreen and New assets

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../screens/login_screen.dart';
 import '../screens/signup_screen.dart';
+import 'package:flutter/services.dart';
+
 
 class WelcomeScreen extends StatefulWidget {
   WelcomeScreen({Key key, this.title}) : super(key: key);
@@ -11,57 +13,117 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
+
+
 class _WelcomeScreenState extends State<WelcomeScreen> {
 
-  Widget _submitButton() {
+  Widget boton_cliente(){
     return InkWell(
       onTap: () {
-        
+       //ruta a la pagina de cliente
       },
       child: Container(
         width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.symmetric(vertical: 13),
+        //padding: EdgeInsets.symmetric(vertical: 13),
         alignment: Alignment.center,
         decoration: BoxDecoration(
-          color: Theme.of(context).accentColor,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Text(
-           'Quiero ofrecer servicios',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.w700),
-        ),
-      ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 3,
+                  )
+                ],
+              ),
+              // padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: ListTile(
+                contentPadding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                leading: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.wc,
+                    ),
+                  ],
+                ),
+                title: Text(
+                  "Cliente",
+                  style: TextStyle(
+                    color: Theme.of(context).accentColor,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+                subtitle: Text(
+                  "Contrata servicios, rápido,\nconfiable",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                
+                trailing: Icon(
+                      Icons.arrow_forward_ios
+                  ),
+
+              ),
+            ),
     );
   }
 
-  Widget _signUpButton() {
+  Widget boton_oferente(){
     return InkWell(
       onTap: () {
-        
+        // ruta a la pagina de oferente
       },
       child: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.symmetric(vertical: 13),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Theme.of(context).accentColor,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Text(
-           'Quiero pedir servicios',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.w700),
-        ),
-      ),
+              width: MediaQuery.of(context).size.width,
+        //padding: EdgeInsets.symmetric(vertical: 13),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 3,
+                  )
+                ],
+              ),
+              // padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+              child: ListTile(
+                contentPadding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                leading: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.work,
+                    ),
+                  ],
+                ),
+                title: Text(
+                  "Oferente",
+                  style: TextStyle(
+                    color: Theme.of(context).accentColor,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+                subtitle: Text(
+                  "Ofrece tus servicios y gana\ndinero",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                trailing: Icon(
+                      Icons.arrow_forward_ios
+                  ),
+
+              ),
+            ),
     );
   }
+  
 
 Widget _accountLabel() {
     return InkWell(
@@ -70,7 +132,7 @@ Widget _accountLabel() {
             context, MaterialPageRoute(builder: (context) => LoginScreen()));
       },
       child: Container(
-        margin: EdgeInsets.symmetric(vertical: 20),
+       // margin: EdgeInsets.symmetric(vertical: 20),
         padding: EdgeInsets.all(15),
         alignment: Alignment.bottomCenter,
         child: Row(
@@ -100,72 +162,103 @@ Widget _accountLabel() {
   @override
   Widget build(BuildContext context) {
    
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-            
-           
-      body:SingleChildScrollView(
-       
-        child:Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            height: MediaQuery.of(context).size.height,
-            decoration: new BoxDecoration(borderRadius: BorderRadius.circular(100),
-              color: Theme.of(context).primaryColor),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold( 
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Column(
               children: <Widget>[
-               
                 Container(
-                  margin: EdgeInsets.symmetric(vertical: 15),
-                  padding: EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                      //crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        /*
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                           Text(
-                              '¡Únetenos!',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  color: Colors.blue[900],
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w900),
-                            ),
-                          ]
-                        ),
-                        */ 
-                       Image.network('https://i.ibb.co/PWc0tBq/Logo-Ser-Conexion.jpg'),
-                       
-                       SizedBox(
-                         height: 80,
-                       ),
-                       _signUpButton(),
-
-                       SizedBox(
-                         height: 20,
-                       ),
-                       _submitButton(),
-                       
-                       SizedBox(
-                         height: 80,
-                       ),
-
-                       _accountLabel()
-
-                      ],
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30.0),
+                      bottomRight: Radius.circular(30.0),
                     ),
-                )
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  height: MediaQuery.of(context).size.height * .60,
+                  padding: EdgeInsets.only(top: 20, left: 30, right: 20),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                        new Image.asset(
+                      'assets/isotipo_transpatente.jpg'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                        new Image.asset(
+                      'assets/Imagen_welcome.jpg'),
+                        ],
+                      ),
+
+                      RichText(
+                        text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            
+                            text: 'Bienvenido a la ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 28,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'plataforma de servicios',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 28,
+                            ),
+                          )
+                        ]),
+                  ),
+                    ]
+                  ),
+
+                ),
+                Container( 
+                  padding: EdgeInsets.only(top: 20, left: 30, right: 20), 
+                  child: Column(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            
+                            text: 'Elige como quieres registrarte',
+                            style: TextStyle(
+                              color: Theme.of(context).accentColor,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ]),
+                  ),
+                  SizedBox(
+                         height: 15,
+                       ),
+                  boton_cliente(),
+                  SizedBox(
+                         height: 10,
+                       ),
+                  boton_oferente(),
+                  SizedBox(
+                         height: 10,
+                       ),
+                  _accountLabel()
+                    ],
+                  ),
+                ),
               ],
             ),
-          ),
+          ],
+        ),
       ),
     );
   }
